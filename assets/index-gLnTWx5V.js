ad02752827b8c89cfe9dd37bf6b662ab61b331e2
ph-1dnjs <ph.1dnjs@gmail.com>
2025-03-27 16:19:25 +0900

    Updates

--- assets/index-gLnTWx5V.js
+++ assets/index-gLnTWx5V.js
@@ -1,31 +1,31 @@
-(function(){const n=document.createElement("link").relList;if(n&&n.supports&&n.supports("modulepreload"))return;for(const o of document.querySelectorAll('link[rel="modulepreload"]'))r(o);new MutationObserver(o=>{for(const t of o)if(t.type==="childList")for(const l of t.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&r(l)}).observe(document,{childList:!0,subtree:!0});function s(o){const t={};return o.integrity&&(t.integrity=o.integrity),o.referrerPolicy&&(t.referrerPolicy=o.referrerPolicy),o.crossOrigin==="use-credentials"?t.credentials="include":o.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function r(o){if(o.ep)return;o.ep=!0;const t=s(o);fetch(o.href,t)}})();const c="user",b=e=>{localStorage.setItem(c,JSON.stringify(e))},u=()=>{const e=localStorage.getItem(c);return e?JSON.parse(e):null},v=()=>{localStorage.removeItem(c)},h=(e,n="",s="")=>{const r={username:e,email:n,bio:s};return b(r),r},x=e=>{const n=u();if(!n)return null;const s={...n,...e};return b(s),s},y=()=>!!u(),i={createUser:h,updateUser:x,getUserFromLocalStorage:u,removeUserFromLocalStorage:v,isLoggedIn:y},d="/front_5th_chapter1-1/",p=()=>{const e=i.isLoggedIn(),n=e?'<li><a id="logout" href="/login" class="text-gray-600">로그아웃</a></li>':'<li><a href="/login" class="text-gray-600">로그인</a></li>',s=location.hash.replace("#","")||"/",r=(o,t)=>o===t?"text-blue-600":"text-gray-600";return`
-    <header class="bg-blue-600 text-white p-4 sticky top-0">
-      <h1 class="text-2xl font-bold">항해플러스</h1>
-    </header>
-   
-    <nav class="bg-white shadow-md p-2 sticky top-14">
-      <ul class="flex justify-around">
-        <li><a href="/" class="${r(s,"/")}">홈</a></li>
-       ${e?`<li><a href="/profile" class="${r(s,"/profile")}">프로필</a></li>`:""}
-        ${n}
-      </ul>
-    </nav>`},f=()=>`
+(function(){const n=document.createElement("link").relList;if(n&&n.supports&&n.supports("modulepreload"))return;for(const o of document.querySelectorAll('link[rel="modulepreload"]'))l(o);new MutationObserver(o=>{for(const s of o)if(s.type==="childList")for(const t of s.addedNodes)t.tagName==="LINK"&&t.rel==="modulepreload"&&l(t)}).observe(document,{childList:!0,subtree:!0});function r(o){const s={};return o.integrity&&(s.integrity=o.integrity),o.referrerPolicy&&(s.referrerPolicy=o.referrerPolicy),o.crossOrigin==="use-credentials"?s.credentials="include":o.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function l(o){if(o.ep)return;o.ep=!0;const s=r(o);fetch(o.href,s)}})();const m="user",f=e=>{localStorage.setItem(m,JSON.stringify(e))},b=()=>{const e=localStorage.getItem(m);return e?JSON.parse(e):null},h=()=>{localStorage.removeItem(m)},x=(e,n="",r="")=>{const l={username:e,email:n,bio:r};return f(l),l},y=e=>{const n=b();if(!n)return null;const r={...n,...e};return f(r),r},w=()=>!!b(),a={createUser:x,updateUser:y,getUserFromLocalStorage:b,removeUserFromLocalStorage:h,isLoggedIn:w},p=()=>{const e=a.isLoggedIn(),n=e?'<li><a id="logout" href="/login" class="text-gray-600">로그아웃</a></li>':'<li><a href="/login" class="text-gray-600">로그인</a></li>',r=location.hash.replace("#","")||"/",l=(o,s)=>o===s?"text-blue-600":"text-gray-600";return`
+      <header class="bg-blue-600 text-white p-4 sticky top-0">
+        <h1 class="text-2xl font-bold">항해플러스</h1>
+      </header>
+     
+      <nav class="bg-white shadow-md p-2 sticky top-14">
+        <ul class="flex justify-around">
+          <li><a href="/" class="${l(r,"/")}">홈</a></li>
+         ${e?`<li><a href="/profile" class="${l(r,"/profile")}">프로필</a></li>`:""}
+          ${n}
+        </ul>
+      </nav>`},g=()=>`
   <footer class="bg-gray-200 p-4 text-center">
     <p>&copy; 2024 항해플러스. All rights reserved.</p>
   </footer>
-`,w=()=>`
-  <div class="mb-4 bg-white rounded-lg shadow p-4">
-    <textarea class="w-full p-2 border rounded" placeholder="무슨 생각을 하고 계신가요?"></textarea>
-    <button class="mt-2 bg-blue-600 text-white px-4 py-2 rounded">게시</button>
-  </div>
-`,L=()=>`
+`;function L(){return`
+<div class="mb-4 bg-white rounded-lg shadow p-4">
+  <textarea class="w-full p-2 border rounded" placeholder="무슨 생각을 하고 계신가요?"></textarea>
+  <button class="mt-2 bg-blue-600 text-white px-4 py-2 rounded">게시</button>
+</div>
+`}const I=()=>`
   <div id="root">
     <div class="bg-gray-100 min-h-screen flex justify-center">
       <div class="max-w-md w-full">
         ${p()}
   
         <main class="p-4">
-          ${i.isLoggedIn()?w():""}
+          ${a.isLoggedIn()?L():""}
   
           <div class="space-y-4">
   
@@ -111,51 +111,11 @@
           </div>
         </main>
   
-        ${f()}
+        ${g()}
       </div>
     </div>
   </div>
-`,I=()=>`
-  <div id="root">
-    <main class="bg-gray-100 flex items-center justify-center min-h-screen">
-      <div class="bg-white p-8 rounded-lg shadow-md w-full text-center" style="max-width: 480px">
-        <h1 class="text-2xl font-bold text-blue-600 mb-4">항해플러스</h1>
-        <p class="text-4xl font-bold text-gray-800 mb-4">404</p>
-        <p class="text-xl text-gray-600 mb-8">페이지를 찾을 수 없습니다</p>
-        <p class="text-gray-600 mb-8">
-          요청하신 페이지가 존재하지 않거나 이동되었을 수 있습니다.
-        </p>
-        <a href="/" class="bg-blue-600 text-white px-4 py-2 rounded font-bold">
-          홈으로 돌아가기
-        </a>
-      </div>
-    </main>
-  </div>
-`,S=()=>`
-  <div id="root">
-    <main class="bg-gray-100 flex items-center justify-center min-h-screen">
-      <div class="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
-        <h1 class="text-2xl font-bold text-center text-blue-600 mb-8">항해플러스</h1>
-        <form id="login-form">
-          <div class="mb-4">
-            <input id="username" type="text" placeholder="사용자 이름" class="w-full p-2 border rounded">
-          </div>
-          <div class="mb-6">
-            <input type="password" placeholder="비밀번호" class="w-full p-2 border rounded">
-          </div>
-          <button id="login-btn" type="submit" class="w-full bg-blue-600 text-white p-2 rounded font-bold">로그인</button>
-        </form>
-        <div class="mt-4 text-center">
-          <a href="#" class="text-blue-600 text-sm">비밀번호를 잊으셨나요?</a>
-        </div>
-        <hr class="my-6">
-        <div class="text-center">
-          <button class="bg-green-500 text-white px-4 py-2 rounded font-bold">새 계정 만들기</button>
-        </div>
-      </div>
-    </main>
-  </div>
-`,$=()=>{const e=i.getUserFromLocalStorage();return`
+`,S=()=>{const e=a.getUserFromLocalStorage();return`
     <div id="root">
       <div class="bg-gray-100 min-h-screen flex justify-center">
         <div class="max-w-md w-full">
@@ -219,8 +179,48 @@
             </div>
           </main>
 
-          ${f()}
+          ${g()}
         </div>
       </div>
     </div>
-  `},a=function(){const e={};function n(t,l){e[t]=l}function s(t){if(t=t.replace(d,"/"),!i.isLoggedIn()&&t==="/profile")return s("/login");if(i.isLoggedIn()&&t==="/login")return s("/");(e[t]||e["/404"])()}function r(){let t=window.location.hash.replace("#","")||window.location.pathname;t=t.replace(d,"/"),!i.isLoggedIn()&&t==="/profile"&&(t="/login"),i.isLoggedIn()&&t==="/login"&&(t="/"),(e[t]||e["/404"])()}function o(){window.addEventListener("hashchange",r),window.addEventListener("load",r),window.addEventListener("popstate",()=>{const t=location.pathname.replace(d,"/");if(t==="/login"&&i.isLoggedIn()){history.go(1);return}s(t)}),window.addEventListener("click",function(t){const l=t.target.getAttribute("href");if(l){if(t.preventDefault(),history.pushState(null,"",`#${l}`),t.target.id==="logout"){i.removeUserFromLocalStorage(),e["/login"]();return}(e[l]||e["/404"])()}}),window.addEventListener("submit",function(t){if(t.preventDefault(),t.target.id==="login-form"){const l=document.getElementById("username").value;i.createUser(l),history.pushState(null,"","#/profile"),e["/profile"]()}else if(t.target.id==="profile-form"){const l=document.getElementById("username").value,m=document.getElementById("email").value,g=document.getElementById("bio").value;i.updateUser({username:l,email:m,bio:g}),alert("프로필이 업데이트되었습니다.")}}),s(location.pathname)}return{addRoute:n,navigate:s,init:o}}();a.addRoute("/",()=>{document.body.innerHTML=L()});a.addRoute("/profile",()=>{document.body.innerHTML=$()});a.addRoute("/login",()=>{document.body.innerHTML=S()});a.addRoute("/404",()=>{document.body.innerHTML=I()});a.init();
+  `},$=()=>`
+  <div id="root">
+    <main class="bg-gray-100 flex items-center justify-center min-h-screen">
+      <div class="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
+        <h1 class="text-2xl font-bold text-center text-blue-600 mb-8">항해플러스</h1>
+        <form id="login-form">
+          <div class="mb-4">
+            <input id="username" type="text" placeholder="사용자 이름" class="w-full p-2 border rounded">
+          </div>
+          <div class="mb-6">
+            <input type="password" placeholder="비밀번호" class="w-full p-2 border rounded">
+          </div>
+          <button id="login-btn" type="submit" class="w-full bg-blue-600 text-white p-2 rounded font-bold">로그인</button>
+        </form>
+        <div class="mt-4 text-center">
+          <a href="#" class="text-blue-600 text-sm">비밀번호를 잊으셨나요?</a>
+        </div>
+        <hr class="my-6">
+        <div class="text-center">
+          <button class="bg-green-500 text-white px-4 py-2 rounded font-bold">새 계정 만들기</button>
+        </div>
+      </div>
+    </main>
+  </div>
+`,E=()=>`
+<div id="root">
+  <main class="bg-gray-100 flex items-center justify-center min-h-screen">
+    <div class="bg-white p-8 rounded-lg shadow-md w-full text-center" style="max-width: 480px">
+      <h1 class="text-2xl font-bold text-blue-600 mb-4">항해플러스</h1>
+      <p class="text-4xl font-bold text-gray-800 mb-4">404</p>
+      <p class="text-xl text-gray-600 mb-8">페이지를 찾을 수 없습니다</p>
+      <p class="text-gray-600 mb-8">
+        요청하신 페이지가 존재하지 않거나 이동되었을 수 있습니다.
+      </p>
+      <a href="/" class="bg-blue-600 text-white px-4 py-2 rounded font-bold">
+        홈으로 돌아가기
+      </a>
+    </div>
+  </main>
+</div>
+`,u="/front_5th_chapter1-1",d=function(){const e={};function n(t,i){e[t]=i}function r(t){const i=e[t]||e["/404"],c=e[t]?t:"/404";history.pushState(null,"",`${u}${c}`),i()}function l(t){if(t=t.replace(u,""),!a.isLoggedIn()&&t==="/profile")return l("/login");if(a.isLoggedIn()&&t==="/login")return l("/");r(t)}function o(){let t=window.location.hash.replace("#","")||window.location.pathname;t=t.replace(u,""),!a.isLoggedIn()&&t==="/profile"&&(t="/login"),a.isLoggedIn()&&t==="/login"&&(t="/"),r(t)}function s(){window.addEventListener("hashchange",o),window.addEventListener("load",o),window.addEventListener("popstate",()=>{const t=location.pathname.replace(u,"");if(t==="/login"&&a.isLoggedIn()){history.go(1);return}l(t)}),window.addEventListener("click",function(t){const i=t.target.getAttribute("href");if(i){if(t.preventDefault(),history.pushState(null,"",`#${i}`),t.target.id==="logout"){a.removeUserFromLocalStorage(),e["/login"]();return}(e[i]||e["/404"])()}}),window.addEventListener("submit",function(t){if(t.preventDefault(),t.target.id==="login-form"){const i=document.getElementById("username").value;a.createUser(i),history.pushState(null,"","#/profile"),e["/profile"]()}else if(t.target.id==="profile-form"){const i=document.getElementById("username").value,c=document.getElementById("email").value,v=document.getElementById("bio").value;a.updateUser({username:i,email:c,bio:v}),alert("프로필이 업데이트되었습니다.")}}),l(location.pathname)}return{addRoute:n,navigate:l,init:s}}();d.addRoute("/",()=>{document.body.innerHTML=I()});d.addRoute("/profile",()=>{document.body.innerHTML=S()});d.addRoute("/login",()=>{document.body.innerHTML=$()});d.addRoute("/404",()=>{document.body.innerHTML=E()});d.init();
