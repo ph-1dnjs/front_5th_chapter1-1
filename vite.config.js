9709fc008d545bd2432506edf774dc20af411177
ph-1dnjs <ph.1dnjs@gmail.com>
2025-03-28 00:14:12 +0900

    해시 라우터 추가 설정

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from "vitest/config";
 
+import { resolve } from "path";
+
 const isProduction = process.env.NODE_ENV === "production";
 
 export default defineConfig({
   base: isProduction ? "/front_5th_chapter1-1/" : "/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        hash: resolve(__dirname, "index.hash.html"),
+      },
+    },
+  },
   test: {
     globals: true,
     environment: "jsdom",
